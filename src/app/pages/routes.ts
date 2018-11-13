381e70dac2c152e0d0a957396b53755d39deaccb
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-11-13 09:39:43 +0100

    fix: add type for routes

--- src/app/pages/routes.ts
+++ src/app/pages/routes.ts
@@ -1,7 +1,8 @@
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { Routes } from '@angular/router';
 
-export const routes = [
+export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent }
 ];
